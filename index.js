22e9390fdc2ae6621c23f79d5bb0ec44b932a818
Victor Yves Crispim <rotcivyves@gmail.com>
2019-04-10 21:39:44 -0300

    Enviando mensagem de teste

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var admin = require("firebase-admin");
-
 var serviceAccount = "./secrets/firebase-serviceaccount-key.json";
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://groupmatch-f14e4.firebaseio.com"
 });
+
+let registrationToken = "dMc9NfpnPzg:APA91bFuXLUHemzR4aw0BJdqRfKoyaMYGzsaqLhp-tFHPWAY_zLGI0DJrm8bAq-UpoSzU1Hp-mQ_N3C9iwaHhUMsONLjepxwPlDO86-r0yQzmHN-ASxYL5QRBVxY4cN6qM0baLg_3b1U";
+let payload = {
+    notification: {
+      title: "Account Deposit",
+      body: "A deposit to your savings account has just cleared."
+    }
+  };
+
+  admin.messaging().sendToDevice(registrationToken, payload)
+  .then(function(response) {
+    console.log("Successfully sent message:", response);
+  })
+  .catch(function(error) {
+    console.log("Error sending message:", error);
+  });
