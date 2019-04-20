54aa0ab8413aa2820bc99c7e4dd4b744c32fe75f
Victor Yves Crispim <rotcivyves@gmail.com>
2019-04-20 19:06:20 -0300

    Extrai rotas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import db from './db/db';
 import bodyParser from 'body-parser';
+import router from './routes/index.js';
 
 // Set up the express app
 const app = express();
@@ -8,15 +9,9 @@ const app = express();
 //Parse incoming request data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
-// get all todos
-app.get('/api/v1/todos', (req, res) => {
-  res.status(200).send({
-    success: 'true',
-    message: 'todos retrieved successfully',
-    todos: db
-  })
-});
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
---
+++ routes/index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import db from '../db/db';
+
+const router = express.Router();
+
+// get all todos
+router.get('/api/v1/todos', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'todos retrieved successfully',
+        todos: db
+    })
+});
+
+router.post('/api/v1/todos', (req, res) => {
+    if (!req.body.title) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'title is required'
+        });
+    } else if (!req.body.description) {
+        return res.status(400).send({
+            success: 'false',
+            message: 'description is required'
+        });
+    }
+    const todo = {
+        id: db.length + 1,
+        title: req.body.title,
+        description: req.body.description
+    }
+    db.push(todo);
+    return res.status(201).send({
+        success: 'true',
+        message: 'todo added successfully',
+        todo
+    })
+});
+
+export default router;
