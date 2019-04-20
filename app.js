014985f6d75be9aba115bd91c1f5c60fe78cbd1f
Victor Yves Crispim <rotcivyves@gmail.com>
2019-04-20 18:47:44 -0300

    Configura body-parser

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 import express from 'express';
 import db from './db/db';
+import bodyParser from 'body-parser';
+
 // Set up the express app
 const app = express();
+
+//Parse incoming request data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // get all todos
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
