b30123a9ccfd27d1b8b54a4e86b24e00b82adb22
Victor Yves Crispim <rotcivyves@gmail.com>
2019-05-30 23:52:19 -0300

    Cria GetUser

---
+++ actions/GetUser.js
@@ -0,0 +1,27 @@
+import db from "../db/firestore-db";
+
+module.exports = class GetUser {
+    get(userId) {
+        return new Promise(function (resolve, reject) {
+            let usersCollection = db.collection("users");
+            let getUser = usersCollection.doc(userId).get();
+            getUser.then(user => {
+                console.log(user);
+                if (user.exists) {
+                    resolve(user.data);
+                } else {
+                    reject({
+                        code: 417,
+                        message: `User with id ${userId} does not exist.`
+                    });
+                }
+            }).catch(error => {
+                console.log(error);
+                reject({
+                    code: 500,
+                    message: `Failed to retrieve user with id ${userId} from database.`
+                });
+            });
+        });
+    }
+}
