347a131233b622a8c41f1fcf33ed01d6318ce919
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-03 22:44:41 -0300

    Move registro do listener para arquivo do db

--- db/firestore-db.js
+++ db/firestore-db.js
@@ -1,10 +1,26 @@
 import firebaseAdmin from "firebase-admin";
-const serviceAccount = "./secrets/firebase-serviceaccount-key.json";
+let serviceAccount = "./secrets/firebase-serviceaccount-key.json";
+import userCreatedListener from "./UserCreatedListener";
 
 firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(serviceAccount),
     databaseURL: "https://groupmatch-f14e4.firebaseio.com"
 });
 
-const db = firebaseAdmin.firestore();
+let db = firebaseAdmin.firestore();
+let usersCollection = db.collection("users");
+
+usersCollection.onSnapshot(snapshot => {
+    console.log(`Snapshot Timestamp ${snapshot.readTime.toMillis()}`)
+    snapshot.docChanges().forEach(change => {
+        console.log(`Document Timestamp ${change.doc.createTime.toMillis()}`)
+        console.log(snapshot.readTime.toMillis() < change.doc.createTime.toMillis())
+        if (change.type == "added" && change.doc.createTime.toMillis() > snapshot.readTime.toMillis()) {
+            let userData = change.doc.data();
+            console.log(user);
+            userCreatedListener.onUserCreated(change.doc.id, userData.tokens);
+        }
+    });
+});
+
 export default db;
