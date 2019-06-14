4ac20218ba74c899ad696e77598251ab88bd6a35
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-13 23:43:37 -0300

    Implementa onGroupCreated

--- db/GroupCreatedListener.js
+++ db/GroupCreatedListener.js
@@ -1,12 +1,20 @@
 import { pushService } from "../services/push-service";
 import { getUsers } from "../actions/get_user"
 
+
 class GroupCreatedListener {
 
-    onGroupCreated(group) { 
+    onGroupCreated(group) {
         console.log(group);
-        getUsers(group.members).then(users => {
-            console.log(users);
+        getUsers(group.members).then(userDocs => {
+            const deviceTokens = userDocs
+                .filter(userDoc => { return userDoc.id != group.admins[0] })
+                .map(userDoc => { return userDoc.data().tokens.device });
+            console.log(deviceTokens);
+
+            const title = `${group.name}`;
+            const body = `Você foi adicionado ao grupo por ${group.admins[0]}`
+            pushService.sendMulticast(title, body, deviceTokens)
         }).catch(error => {
             console.log(error);
         });
