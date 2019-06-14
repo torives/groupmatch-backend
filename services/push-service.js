040af7507268f63e08c57ea82b4e3e61242c1955
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-13 23:50:44 -0300

    Refactor push-service

--- services/push-service.js
+++ services/push-service.js
@@ -17,19 +17,3 @@ export function sendMulticast(title, body, deviceTokens) {
             console.log(error);
         });
 }
-
-// const registrationToken = "dMc9NfpnPzg:APA91bFuXLUHemzR4aw0BJdqRfKoyaMYGzsaqLhp-tFHPWAY_zLGI0DJrm8bAq-UpoSzU1Hp-mQ_N3C9iwaHhUMsONLjepxwPlDO86-r0yQzmHN-ASxYL5QRBVxY4cN6qM0baLg_3b1U";
-// const payload = {
-//     notification: {
-//       title: "Account Deposit",
-//       body: "A deposit to your savings account has just cleared."
-//     }
-//   };
-
-//   admin.messaging().sendToDevice(registrationToken, payload)
-//   .then(function(response) {
-//     console.log("Successfully sent message:", response);
-//   })
-//   .catch(function(error) {
-//     console.log("Error sending message:", error);
-//   });
