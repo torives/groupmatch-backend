823f957ef95e7b52e87f3e1b57449157d6429a4a
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-16 17:02:19 -0300

    Observa a criação de matches

--- db/firestore_db.js
+++ db/firestore_db.js
@@ -1,8 +1,11 @@
 import { firebaseAdmin } from "../services/firebase_service";
 import { userCreatedListener } from "./UserCreatedListener";
 import { groupCreatedListener } from "./GroupCreatedListener";
+import { matchCreatedListener } from "./MatchCreatedListener";
 
 const firestore = firebaseAdmin.firestore();
+
+//TODO: extract observation logic to a function
 
 const usersCollection = firestore.collection("users");
 usersCollection.onSnapshot(snapshot => {
@@ -22,6 +25,17 @@ groupsCollection.onSnapshot(snapshot => {
             const groupData = change.doc.data();
             console.log(groupData);
             groupCreatedListener.onGroupCreated(groupData);
+        }
+    });
+});
+
+const matchesCollection = firestore.collection("matches");
+matchesCollection.onSnapshot(snapshot => {
+    snapshot.docChanges().forEach(change => {
+        if (change.type == "added" && change.doc.createTime.toMillis() >= snapshot.readTime.toMillis()) {
+            const matchData = change.doc.data();
+            console.log(matchData);
+            matchCreatedListener.onMatchCreated(matchData);
         }
     });
 });
