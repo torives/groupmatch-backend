59ebff27ea65f9174603a308b787898421a41466
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-16 23:19:13 -0300

    Corrige validação

--- controllers/MatchController.js
+++ controllers/MatchController.js
@@ -3,27 +3,46 @@ const { body, validationResult } = require('express-validator/check');
 
 class MatchController {
 
-    createMatch(req, res) { }
+    createMatch(req, res) {
+        console.log(req.body);
 
-    updateMatch(req, res) { }
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).send({
+                errors: errors.array()
+            });
+        }
+    }
+
+    updateMatch(req, res) {
+        console.log(req.body);
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).send({
+                errors: errors.array()
+            });
+        }
+    }
 
     validate(method) {
         switch (method) {
             case "createMatch": {
                 return [
                     body("groupId").exists(),
-                    body("status").exists().isIn(["CREATED", "ONGOING", "FINISHED"], "Invalid status. Must be: ONGOING or FINISHED"),
+                    body("status")
+                        .isIn(["CREATED", "ONGOING", "FINISHED"])
+                        .withMessage(),
                     body("created_by").exists(),
-                    body("created_at").exists().isRFC3339(),
-                    body("participants").exists().isArray(),
-                    body("answers").exists().isArray()
+                    body("participants").isArray(),
+                    body("answers").isArray()
                 ]
             }
             case "updateMatch": {
                 return [
                     body("id").exists().isString(),
                     body("has_joined").exists().isBoolean(),
-                    body("local_calendar").optional().isJSON()
+                    body("local_calendar").optional()
                 ]
             }
         }
