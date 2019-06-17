7c1ae9498abbd1403dbe352b94c57f1e9559fd0c
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-16 21:38:22 -0300

    Implementa onMatchCreated

--- db/MatchCreatedListener.js
+++ db/MatchCreatedListener.js
@@ -1,8 +1,21 @@
+import { getUsers } from "../actions/get_user";
+import { getCalendar } from "../actions/get_calendar"
+
 class MatchCreatedListener {
 
-    onMatchCreated(match) {}
+    async onMatchCreated(match) {
+        const userIds = match.participants.map(participant => { participant.id })
+        console.log(userIds);
+        try {
+            const users = await getUsers(userIds);
+            const userCalendar = await getCalendar(users[0]);
+            console.log(userCalendar);
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-    onMatchUpdated(match){}
+    onMatchUpdated(match) { }
 }
 
 export const matchCreatedListener = new MatchCreatedListener();
