c785e499ff91a7d0e96815c5455661d178a000ca
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-16 21:38:58 -0300

    Altera criação do calendarClient

--- services/google_service.js
+++ services/google_service.js
@@ -15,4 +15,27 @@ export const authClient = new google.auth.OAuth2(
     keys.redirect_uris[0]
 );
 
-export const calendarClient = google.calendar({version: 'v3', authClient});
+export function getCalendarClient(credentials) {
+    authClient.credentials = credentials;
+    return google.calendar({ version: 'v3', authClient });
+}
+
+// calendarClient.events.list({
+//     calendarId: 'primary',
+//     timeMin: (new Date()).toISOString(),
+//     maxResults: 10,
+//     singleEvents: true,
+//     orderBy: 'startTime',
+// }, (err, res) => {
+//     if (err) return console.log('The API returned an error: ' + err);
+//     const events = res.data.items;
+//     if (events.length) {
+//         console.log('Upcoming 10 events:');
+//         events.map((event, i) => {
+//             const start = event.start.dateTime || event.start.date;
+//             console.log(`${start} - ${event.summary}`);
+//         });
+//     } else {
+//         console.log('No upcoming events found.');
+//     }
+// });
