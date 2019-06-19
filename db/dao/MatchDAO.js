53931aad33eb06e1fda77df0b761386c678d99a6
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-18 22:26:30 -0300

    Implementa createMatch
    Implementa MatchDAO

--- db/dao/MatchDAO.js
+++ db/dao/MatchDAO.js
@@ -9,17 +9,55 @@ class MatchDAO {
         return new Promise(function (resolve, reject) {
             matchesCollection.add(match)
                 .then(result => {
-                    
+                    console.log(`[MatchDAO]: successfully created match`);
+                    resolve(true);
                 }).catch(error => {
-
+                    console.log(`[MatchDAO]: failed to create match.`, error);
+                    reject({
+                        code: 500,
+                        message: `Failed to insert match into the database.`
+                    })
                 })
         });
     }
 
-    updateMatch(match) {
-        return new Promise(function (resolve, reject) { });
+    getMatch(matchId) {
+        return new Promise(function (resolve, reject) {
+            matchesCollection.doc(matchId).get().then(match => {
+                if (match.exists) {
+                    console.log(`[MatchDAO]: successfully retrieved match with id ${matchId}`);
+                    resolve(group);
+                } else {
+                    console.log(`[MatchDAO]: failed to retrieve match with id ${matchId}. Match does not exist.`);
+                    reject({
+                        code: 422,
+                        message: `Match with id ${matchId} does not exist.`
+                    });
+                }
+            }).catch(error => {
+                console.log(`[MatchDAO]: failed to retrieve match with id ${matchId}`, error);
+                reject({
+                    code: 500,
+                    message: `Failed to retrieve match with id ${matchId} from database.`
+                });
+            });
+        })
+    }
+
+    updateMatch(matchId, matchData) {
+        return new Promise(function (resolve, reject) {
+            matchesCollection.doc(matchId).set(matchData, { merge: true }).then(result => {
+                console.log(`[MatchDAO]: successfully updated match with id ${matchId}`);
+                resolve(true);
+            }).catch(error => {
+                console.log(`MatchDAO: failed to update group with id ${matchId}`, error);
+                reject({
+                    code: 500,
+                    message: `Failed to update group with id ${matchId} to the database.`
+                })
+            });
+        });
     }
 }
-
 
 export const matchDAO = new MatchDAO();
