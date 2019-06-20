c8815798cfb3a4e206cb019ce8a727adda84ce6c
Victor Yves Crispim <rotcivyves@gmail.com>
2019-06-20 15:13:47 -0300

    Corrige chamada de métodos no MatchListener

--- db/MatchListener.js
+++ db/MatchListener.js
@@ -1,14 +1,14 @@
 import { userDAO } from "./dao/UserDAO";
-import { getCalendar } from "../actions/get_calendar"
+import { calendarDAO } from "../db/dao/CalendarDAO"
 
 class MatchListener {
 
     async onMatchCreated(match) {
-        const userIds = match.participants.map(participant => { participant.id })
+        const userIds = match.participants.map(participant => participant.id);
         console.log(userIds);
         try {
             const users = await userDAO.getUsers(userIds);
-            const userCalendar = await getCalendar(users[0]);
+            const userCalendar = await calendarDAO.getCalendar(users[0].data());
             console.log(userCalendar);
         } catch (error) {
             console.log(error);
